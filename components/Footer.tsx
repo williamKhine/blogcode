ef90f00c7104c82aadabc06f3df6d372c15de485
William Khine <william@khine.dev>
2024-06-28 21:04:11 -0400

    update footer

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className={'text-balance text-center text-sm leading-loose text-muted-foreground md:text-left border w-full py-4'}>
+            <p className={'container'}>Copyright © 2024 <a href={'https://khine.dev'} className={'text-blue-500'}>khine.dev</a></p>
+        </footer>
+    )
+}
