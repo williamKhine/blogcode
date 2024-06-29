7e4068c0f8ebb5ea55678c2e7e0d47787fac1f8a
William Khine <william@khine.dev>
2024-06-29 02:18:19 -0400

    Add favicon and svg logo

--- components/BlogcodeLogo.tsx
+++ components/BlogcodeLogo.tsx
@@ -4,7 +4,7 @@ export default function BlogcodeLogo({size}: {size: number}) {
         <svg width={size} height={size} xmlns="http://www.w3.org/2000/svg">
             <rect width={size} height={size} rx={size*0.1} fill="black"/>
             <rect x={pixel} y={pixel} width={2*pixel} height={pixel} fill="white"/>
-            <rect x={3*pixel} y={2*pixel} width={pixel} height={2*pixel} fill="white"/>
+            <rect x={3*pixel} y={2*pixel} width={pixel} height={2*pixel} fill="gray"/>
         </svg>
     )
 }
