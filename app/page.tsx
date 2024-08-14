2292daf5647c7fb468057121c8c48cfa7f3abadf
William Khine <william@khine.dev>
2024-08-14 16:28:25 -0400

    adjust margins for text

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ export default async function Index() {
     return (
         <>
             {user ?
-                <div className={'my-auto'}>
+                <div className={'my-auto container'}>
                     <h1 className={'text-4xl font-bold'}>Welcome back, {user.email}!</h1>
-                    <p className={'text-md mt-2'}>Thank you for signing up!</p>
-                    <p className={'text-md mt-4'}>We will keep you updated.</p>
+                    <p className={'text-md mt-4'}>Thank you for signing up!</p>
+                    <p className={'text-md mt-2'}>We will keep you updated.</p>
                 </div>
                 :
-                <div className={'my-auto'}>
+                <div className={'my-auto container'}>
                     <h1 className={'text-4xl font-bold'}>Welcome to blogcode!</h1>
-                    <p className={'text-md mt-2'}>The platform is still under construction.</p>
-                    <p className={'text-md mt-4'}>Sign up now to receive updates.</p>
+                    <p className={'text-md mt-4'}>The platform is still under construction.</p>
+                    <p className={'text-md mt-2'}>Sign up now to receive updates.</p>
                 </div>
             }
         </>
