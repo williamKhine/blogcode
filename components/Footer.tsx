09cc110459d11d83e333b4dbd96e21e17f2018b5
William Khine <william@khine.dev>
2024-08-14 16:26:46 -0400

    remove bottom borders from footer
    only the top has border now

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className={'text-balance text-center text-sm leading-loose text-muted-foreground md:text-left border w-full py-4'}>
+        <footer className={'text-balance text-center text-sm leading-loose text-muted-foreground md:text-left border-t w-full py-4'}>
             <p className={'container'}>Copyright © 2024 <a href={'https://khine.dev'} target={'_blank'} className={'text-blue-500'}>khine.dev</a></p>
         </footer>
     )
