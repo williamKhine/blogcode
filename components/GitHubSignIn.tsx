98daf4bd9cd9506045c838b72fdcff0675efb3fb
William Khine <william@khine.dev>
2024-08-14 18:48:15 -0400

    initiate supabase outside function
    faster OAuth

--- components/GitHubSignIn.tsx
+++ components/GitHubSignIn.tsx
@@ -3,8 +3,8 @@ import {GitHubLogoIcon} from "@radix-ui/react-icons";
 import {createClient} from "@/utils/supabase/client";
 
 export default function GitHubSignIn() {
+    const supabase = createClient();
     const signInWithGitHub = async () => {
-        const supabase = createClient();
         const {error} = await supabase.auth.signInWithOAuth({
             provider: 'github',
             options: {
